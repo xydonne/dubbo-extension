75fba9307b6abaa57fd82183c175bef2f1c4f8a5
DonneyYoung <xydonne@gmail.com>
2017-10-18 09:46:50 +0800

    init.

--- dubbo-rpc-restexpress/src/main/java/com/alibaba/dubbo/rpc/protocol/restexpress/provider/InvokerBean.java
+++ dubbo-rpc-restexpress/src/main/java/com/alibaba/dubbo/rpc/protocol/restexpress/provider/InvokerBean.java
@@ -4,7 +4,9 @@ import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
 import com.alibaba.dubbo.rpc.RpcInvocation;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.buffer.ByteBuf;
 import org.restexpress.Request;
 
 import java.io.IOException;
@@ -33,11 +35,37 @@ public class InvokerBean<T> {
     }
 
     public Result invoke(String methodName, Request req, Map<String, String> attachments) {
-        Object[] arguments = new Object[1];
         Class<?>[] argumentsClass = METHOD_MAPPING.get(methodName);
-        if (argumentsClass.length > 0)
+        if (argumentsClass.length == 1) {
+            Object[] arguments = new Object[1];
             arguments[0] = req.getBodyAs(argumentsClass[0]);
-        return invoker.invoke(new RpcInvocation(methodName, METHOD_MAPPING.get(methodName), arguments, attachments, null));
+            return invoker.invoke(new RpcInvocation(methodName, METHOD_MAPPING.get(methodName), arguments, attachments, null));
+        } else if (argumentsClass.length > 1) {
+            Object[] arguments = new Object[argumentsClass.length];
+            ByteBuf bf = req.getBody();
+            byte[] byteArray = new byte[bf.capacity()];
+            bf.readBytes(byteArray);
+            String body = new String(byteArray);
+            JsonNode jsonNode;
+            try {
+                jsonNode = MAPPER.readTree(body);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            if (jsonNode.size() != arguments.length) {
+                throw new RuntimeException(String.format("Parameter's size not equal. It should be %s, Yours is %s.", arguments.length, jsonNode.size()));
+            }
+            for (int i = 0; i < arguments.length; i++) {
+                try {
+                    arguments[i] = MAPPER.readValue(jsonNode.get(i).toString(), argumentsClass[i]);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return invoker.invoke(new RpcInvocation(methodName, METHOD_MAPPING.get(methodName), arguments, attachments, null));
+        } else {
+            return invoker.invoke(new RpcInvocation(methodName, METHOD_MAPPING.get(methodName), new Object[0], attachments, null));
+        }
     }
 
     public Result invoke(String methodName, Map<String, String> requestParams, Map<String, String> attachments) {
